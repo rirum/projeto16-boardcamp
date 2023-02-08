808c8cb6702dc49e29f64302c9343156b8f2234c
camille <cerulean.dsgn@gmail.com>
2023-02-08 00:46:33 -0300

    chore: installing pg and defining database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
+// import routers from 
 const PORT = 5000;
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
+// server.use(routers);
 
 // rotas em breve
 // server.use([])
