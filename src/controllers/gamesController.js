84460757b4ccae2db9ee9e6c5641d4d73e5bf1d0
camille <cerulean.dsgn@gmail.com>
2023-02-09 12:43:14 -0300

    fix: validation into post /games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -15,11 +15,18 @@ export async function listarJogos(req,res) {
 export async function inserirJogos(req,res) {
     const {name, image, stockTotal, pricePerDay } = req.body;
     try {
-        // const jogos = await db.query('SELECT * FROM games WHERE name = $1' ,[name,]);
-        const inserir = await db.query('INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)', [name, image, stockTotal, pricePerDay]);
-        if(inserir.rowCount === 0){
+      
+        if(!name || stockTotal <= 0 || pricePerDay <= 0 ){
             return res.sendStatus(400);
         }
+
+        const jogoJaExiste = await db.query('SELECT * FROM games WHERE name = $1' ,[name,]);
+        
+        if(jogoJaExiste.rows.length !== 0){
+            return res.sendStatus(409);
+        }
+
+        await db.query('INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)', [name, image, stockTotal, pricePerDay]);
         res.sendStatus(201);
 
     } catch(error) {
