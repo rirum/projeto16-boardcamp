c17c8b590b1e36f07f0ec1894fe7b010140ac522
camille <cerulean.dsgn@gmail.com>
2023-02-10 14:47:35 -0300

    fix: small fix on customerSchema

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -3,7 +3,7 @@ import joi from 'joi'
 export const customerSchema = joi.object({
     name: joi.string().min(3).required(),
     phone: joi.string().max(11).required(),
-    cpf: joi.number().regex(/^[0-9]{11}$/).required(),
+    cpf: joi.string().regex(/^[0-9]{11}$/).required(),
     birthday: joi.date().required(),
     
 
